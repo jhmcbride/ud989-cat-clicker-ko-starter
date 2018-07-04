c081e961adff3f7bae289e9009c820684d64b658
Jay McBride <jhmcbride@gmail.com>
2018-07-04 20:35:35 +1000

    Moved Cat out of ViewModel
    * Changed some html layout
    * Updated Leveling

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('https://farm6.staticflickr.com/5598/14934282524_344c84246b_q.jpg');
+    this.nicknames = ko.observableArray(['T', 'Tabbitha']);
+
     this.level = ko.computed(function() {
-        if (this.clickCount() >= 10 ) {
-            return 'cat';
-        } if (this.clickCount() < 10) {
-            return 'kitten';
+        var level;
+        var clicks = this.clickCount();
+        if (clicks < 10 ) {
+            level = 'newborn';
+        } else if (clicks < 50) {
+            level = 'kitten';
+        } else {
+            level = 'Cat';
         };
+        return level;
     }, this);
+}
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable( new Cat());
 
     this.incrementCount = function() {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
 };
 
